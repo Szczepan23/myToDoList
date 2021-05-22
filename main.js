b5a0ffc2f4593cb7a96e3f18cc6c22476380a9bc
Rafal Szczepanik <rafal.szczepanik23@gmail.com>
2021-05-22 18:48:02 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+const inputTask = document.querySelector('form');
+const input = document.querySelector('input');
+const ul = document.querySelector('ul');
+const liList = document.getElementsByClassName('task');
+const taskNumber = document.querySelector("h2");
+const searchInput = document.querySelector(".search")
+const liArray = [];
+
+const render = () => {
+    ul.textContent = "";
+    taskNumber.textContent = `Lista zadań do zrobienia: ${liArray.length}`
+    liArray.forEach((element, key) => {
+        element.dataset.key = key;
+        ul.appendChild(element);
+    })
+}
+
+const lineThrough = (e) => {
+    const label = e.target.parentNode.parentNode.querySelector(".label");
+    if (e.target.dataset.active === 'false') {
+        // e.target.parentNode.style.textDecoration = 'line-through';
+        label.classList.add('done');
+        e.target.textContent = 'Done';
+        e.target.dataset.active = 'true';
+    } else {
+        // e.target.parentNode.style.textDecoration = 'none';
+        label.classList.remove('done');
+        e.target.textContent = 'To Do';
+        e.target.dataset.active = 'false';
+    }
+}
+const deleteElement = (e) => {
+    // e.target.parentNode.remove();
+
+    const index = e.target.parentNode.parentNode.dataset.key;
+    liArray.splice(index, 1);
+    console.log(liArray)
+    const taskNumbers = [...document.querySelectorAll('.label')];
+    render();
+
+}
+
+const addTask = (e) => {
+    e.preventDefault();
+    if (input.value === "") return;
+    const task = document.createElement('li');
+    task.className = 'task';
+    liArray.push(task);
+    task.innerHTML = `<span class="label">${input.value}</span><div class="buttons"><button data-active="false" class="check">To Do</button><button class="del">X</button><div>`;
+    input.value = ''; // clean input
+    render();
+    document.querySelectorAll('.check').forEach(item => item.addEventListener('click', lineThrough)); // single 'to do' button
+    document.querySelectorAll('.del').forEach(item => item.addEventListener('click', deleteElement));
+}
+
+const searchTask = (e) => {
+    const mySearch = e.target.value.toLowerCase();
+    const newArray = liArray.filter(li => li.textContent.toLowerCase().includes(mySearch));
+    ul.textContent = "";
+    newArray.forEach(li => ul.appendChild(li))
+
+
+}
+
+inputTask.addEventListener('submit', addTask);
+searchInput.addEventListener('input', searchTask);
