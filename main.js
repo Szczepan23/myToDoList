4cab8c02b5b7b52932cfc0c4471b04d5e3109735
Rafal Szczepanik <rafal.szczepanik23@gmail.com>
2021-05-24 19:18:12 +0200

    change parentNode to closest

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ const input = document.querySelector('input');
 const ul = document.querySelector('ul');
 const taskNumber = document.querySelector("h2");
 const searchInput = document.querySelector(".search")
+const btnAdd = document.querySelector('.add')
 const liArray = [];
 
 const render = () => {
     ul.textContent = "";
-    taskNumber.textContent = `Lista zadań do zrobienia: ${liArray.length}`
+    taskNumber.textContent = `Tasks To Do: ${liArray.length}`
     liArray.forEach((element, key) => {
         element.dataset.key = key;
         ul.appendChild(element);
@@ -17,10 +18,10 @@ const render = () => {
 const lineThrough = ({
     target
 }) => {
-    const label = target.parentNode.parentNode.querySelector(".label");
+    const label = target.closest('li').querySelector(".label");
     if (target.dataset.active === 'false') {
         label.classList.add('done');
-        target.parentNode.parentNode.classList.add('darker');
+        target.closest('li').classList.add('darker');
         target.textContent = 'To Do';
         target.dataset.active = 'true';
     } else {
@@ -36,45 +37,36 @@ const deleteElement = ({
     const index = target.parentNode.parentNode.dataset.key;
     liArray.splice(index, 1);
     render();
-
 }
 
 const stateHandle = () => {
     if (input.value === "") {
-        document.querySelector('.add').disabled = true;
+        btnAdd.disabled = true;
+        btnAdd.classList.add('opa');
     } else {
-        document.querySelector('.add').disabled = false;
+        btnAdd.disabled = false;
+        btnAdd.classList.remove('opa');
     }
 }
-input.addEventListener('change', stateHandle)
+
 const addTask = (e) => {
     e.preventDefault();
     const task = document.createElement('li');
     task.className = 'task';
     liArray.push(task);
-    task.innerHTML = `<span class="label">${input.value}</span><div class="buttons"><button data-active="false" class="check">Done</button><button class="del">&#10005</button><div>`;
+    task.innerHTML = `<span class="label">${input.value}</span><div class="buttons"><button data-active="false" class="checkBtn">Done</button><button class="delBtn">&#10005</button><div>`;
     input.value = ''; // clean input
     render();
-    document.querySelectorAll('.check').forEach(item => item.addEventListener('click', lineThrough)); // single 'to do' button
-    document.querySelectorAll('.del').forEach(item => item.addEventListener('click', deleteElement));
-    document.querySelector('.add').disabled = true;
+    document.querySelectorAll('.checkBtn').forEach(item => item.addEventListener('click', lineThrough)); // single 'to do' button
+    document.querySelectorAll('.delBtn').forEach(item => item.addEventListener('click', deleteElement));
 }
 
-
-
-
-
-
-const searchTask = ({
-    target
-}) => {
-    const mySearch = target.value.toLowerCase();
+const searchTask = (e) => {
+    const mySearch = e.target.value.toLowerCase();
     const newArray = liArray.filter(li => li.textContent.toLowerCase().includes(mySearch));
     ul.textContent = "";
-    newArray.forEach(li => ul.appendChild(li))
-
-
+    newArray.forEach(li => ul.appendChild(li));
 }
-
+input.addEventListener('change', stateHandle);
 inputTask.addEventListener('submit', addTask);
 searchInput.addEventListener('input', searchTask);
